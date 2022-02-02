a8ae0f44b835ca7190c183bcf17d4acd9a5045ef
RobertB <robertburek@wp.pl>
2022-02-02 16:08:09 +0100

    Modyfikacje albumu foto

--- js/foto.js
+++
@@ -1,3 +0,0 @@
-let albumFoto = document.querySelectorAll("#listFoto>li");
-albumFoto = [...albumFoto];
-console.log(albumFoto);
---
+++ js/photo.js
@@ -0,0 +1,9 @@
+let moreFotos = document.getElementById("more-fotos");
+let feewerFotos = document.getElementById("fewer-fotos");
+console.log(feewerFotos);
+
+let fotoHide = document.querySelectorAll("div .foto.hide");
+let fotoShow = document.querySelectorAll("div .foto.show");
+fotoHide = [...fotoHide];
+fotoShow = [...fotoShow];
+console.log(fotoHide);
